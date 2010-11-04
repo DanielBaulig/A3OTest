46276cbc0ae906cdfcbe574f04103ccccf91af38
Daniel Baulig <daniel.baulig@gmx.de>
2010-11-04 23:31:02 +0100

    Add A3MatchZone::isHostileTo test (improve code coverage to 100% on A3MatchZone)

--- Packages/GameState/TestCases/A3/A3MatchZoneTests.php
+++ Packages/GameState/TestCases/A3/A3MatchZoneTests.php
@@ -27,6 +27,8 @@ class A3MatchZoneTestSuite extends PHPUnit_Framework_TestSuite
 	
 		$suite->addTestSuite( 'A3MatchZoneRegistryTests' );
 		$suite->addTestSuite( 'A3MatchZoneFactoryTests' );
+		$suite->addTestSuite( 'A3MatchZoneTests' );
+		
 		
 		return $suite;
 	}
@@ -81,5 +83,28 @@ class A3MatchZoneRegistryTests extends PHPUnit_Framework_TestCase
 				'West Russia'
 			),
 		);
+	}
+}
+
+
+class A3MatchZoneTests extends PHPUnit_Framework_TestCase
+{
+	public function setUp( )
+	{
+		$this->pdo = $this->sharedFixture['pdo'];
+		$this->match = $this->sharedFixture['match_state'];
+	}
+	
+	public function testIsHostile( )
+	{
+		$factory = new A3MatchZonePDOFactory( $this->pdo, $this->match );
+		$zone = $factory->createSingleProduct( 'Archangel' );
+		$this->assertTrue( $zone->isHostileTo( 'Germany', 'infantry' ) );
+		
+		$zone = new A3MatchZone($this->match, array(
+			A3MatchZone::NAME => 'SeaZone',
+			A3MatchZone::PIECES => array( 'Russia' => array( 'factory' => 1 ) )			
+		));
+		$this->assertFalse( $zone->isHostileTo( 'Germany', 'infantry' ) );
 	}
 }
